2184f06b2b6ffcde1c34993d320dfbdb9bcde87f
Julee117 <juliannne@gmail.com>
2017-09-29 14:55:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let x = 0; while x < musicians.length; x++) {
-    array.push(musicians[x] + " plays " + instruments[x]);
+    array.push(`${musicians[x]} plays ${instruments[x]}");
   }
   return array;
 }
